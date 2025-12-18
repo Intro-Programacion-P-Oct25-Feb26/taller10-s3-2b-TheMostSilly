5e89467ef3a49206747b04a5d34d4c970de9f098
TheMostSilly <geovannacastro058@gmail.com>
2025-12-18 11:15:40 -0500

    Ejercicio 03

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 
  */
 package paquete3;
 
@@ -10,5 +8,34 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
-    
+
+    public static void main(String[] args) {
+        boolean[][] sensores = {
+            {true, true, true, true, true, true},
+            {true, false, true, false, true, false},
+            {false, false, false, false, false, false},
+            {true, true, false, true, true, false}
+        };
+        int[] zonas = new int[4];
+        String reporte = "";
+        for (int fila = 0; fila < sensores.length; fila++) {
+            int suma = 0;
+            for (int col = 0; col < sensores[fila].length; col++) {
+                if (sensores[fila][col] == true) {
+                    suma += 1;
+                }
+            }
+            zonas[fila] = suma;
+
+        }
+        for (int fila = 0; fila < zonas.length; fila++) {
+            if (zonas[fila] != 0) {
+                reporte = String.format("%sLa zona %s tiene %s fallas\n", reporte, fila + 1, zonas[fila]);
+            } else {
+                reporte = String.format("%sLa zona %s no tiene fallas\n",reporte, fila + 1);
+            }
+        }
+        System.out.println(reporte);
+    }
+
 }
