e8a1b796d42df1b2e5f26b6938370a2b312454fd
TheMostSilly <geovannacastro058@gmail.com>
2025-12-18 11:58:04 -0500

    EJERCICIO 04

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -17,12 +17,13 @@ public class Problema04 {
         Scanner entrada = new Scanner(System.in);
         int[][] matrizA = new int[3][2];
         int[][] matrizB = new int[3][2];
+        boolean mayor = true;
 
         for (int fila = 0; fila < matrizA.length; fila++) {
 
             for (int col = 0; col < matrizA[fila].length; col++) {
                 System.out.printf("Ingrese los valores de la matriz A en la"
-                        + "posición %s %s\n", fila, col);
+                        + " posición %s %s\n", fila, col);
                 matrizA[fila][col] = entrada.nextInt();
             }
         }
@@ -31,18 +32,37 @@ public class Problema04 {
 
             for (int col = 0; col < matrizB[fila].length; col++) {
                 System.out.printf("Ingrese los valores de la matriz B en la"
-                        + "posición %s %s\n", fila, col);
+                        + " posición %s %s\n", fila, col);
                 matrizB[fila][col] = entrada.nextInt();
             }
 
         }
+        System.out.println("La matriz A tiene los valores");
         for (int fila = 0; fila < matrizA.length; fila++) {
-            System.out.println(matrizA[fila]);
+
+            System.out.printf("%s\t%s\n", matrizA[fila][0], matrizA[fila][1]);
+
+        }
+        System.out.println("La matriz B tiene los valores");
+
+        for (int fila = 0; fila < matrizB.length; fila++) {
+
+            System.out.printf("%s\t%s\n", matrizB[fila][0], matrizB[fila][1]);
 
         }
         for (int fila = 0; fila < matrizB.length; fila++) {
-            System.out.println(matrizB[fila]);
-
+            for (int col = 0; col < matrizB[fila].length; col++) {
+                if (matrizB[fila][col] > matrizA[fila][col]) {
+                    mayor = false;
+                }
+            }
         }
+        if (mayor) {
+            System.out.println("La matriz A es mayor que la matriz B");
+        }else{
+            System.out.println("La matriz A no es mayor a la matriz B");
+        }
+
     }
+
 }
