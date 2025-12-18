027016fe47405eff481e6ace51ec0cb4906e6587
TheMostSilly <geovannacastro058@gmail.com>
2025-12-18 12:09:08 -0500

    Ejercicio 05

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,28 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+        String inicial;
+        String reporte = "";
+        for (int fila = 0; fila < estudiantes.length; fila++) {
+            for (int col = 0; col < estudiantes[fila].length; col++) {
+                String nombre = estudiantes[fila][col];
+                inicial = nombre.substring(0, 1);
+                switch (inicial) {
+                    case ("S"):
+                    case ("P"):
+                    case ("T"):
+                        reporte += String.format("%s\n", nombre);
+                        break;
+                }
+            }
+        }
+        System.out.println(reporte);
+    }
 }
